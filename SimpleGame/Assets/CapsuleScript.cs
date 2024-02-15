feeccbbb0b7190233032f41cef3451c50291002b
KT. Nguyen <kiettn12@gmail.com>
2024-02-15 14:39:38 +0100

    update

--- SimpleGame/Assets/CapsuleScript.cs
+++ SimpleGame/Assets/CapsuleScript.cs
@@ -5,18 +5,28 @@ using UnityEngine;
 public class CapsuleScript : MonoBehaviour
 {
     public Rigidbody2D myRigidBody2;
+    public LogicScript logic;
+    public bool pillIsAlive = true; 
     // Start is called before the first frame update
     void Start()
     {
         gameObject.name = "Blue pill";
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && pillIsAlive == true)
         {
             myRigidBody2.velocity = Vector2.up * 5;
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+        pillIsAlive = false; 
     }
 }
